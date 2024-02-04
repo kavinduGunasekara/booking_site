594f3418f5cb5b93121381919cd575b79f32cb13
WAKD Gunasekara <137909922+kavinduGunasekara@users.noreply.github.com>
2024-02-05 01:54:11 +0530

    config and test USER LOGIN API

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 const App =() =>{
   return (
@@ -26,6 +27,14 @@ const App =() =>{
           </Layout>
           }
           />
+          <Route
+           path="/register" 
+           element= {
+           <Layout>
+            <Register />
+            </Layout>
+             }   
+             />
        
         <Route path="*" element={<Navigate to="/" />} />
         
---
+++ frontend/src/pages/Register.tsx
@@ -0,0 +1,10 @@
+const Register = () => {
+    return (
+    <form className = "flex dlex-col gap-5">
+    <h2 className="text-3xl font-bold"> Create an Account</h2>
+    </form>
+    );
+
+};
+
+export default Register;
