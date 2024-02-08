27adab7333be5cf3090974a9fa968e60e8248766
WAKD Gunasekara <137909922+kavinduGunasekara@users.noreply.github.com>
2024-02-09 00:25:31 +0530

    end reg notification

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,6 +2,7 @@ import { useForm } from "react-hook-form";
 import { useMutation } from "react-query";
 import * as apiClient from '../api-client';
 import { useAppContext } from "../contexts/AppContext";
+import { useNavigate } from "react-router-dom";
 
 export type RegisterFormData ={
     firstName: string,
@@ -12,7 +13,7 @@ export type RegisterFormData ={
 };
 
 const Register = () => {
-
+   const navigate = useNavigate();
     const { showToast } = useAppContext();
 
     const { 
@@ -26,7 +27,7 @@ const Register = () => {
         onSuccess: () => {
 
         showToast({ message:"Registration Success!", type: "SUCCESS" });
-
+          navigate("/");
         },
            onError: (error: Error) => {
    showToast({ message: error.message, type:"ERROR"});
